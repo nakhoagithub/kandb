52d4cac179ce8be065b143e569184f17bb4296d1
Anh Khoa <dev.nakhoa@gmail.com>
2024-03-15 08:59:05 +0700

    Up

--- services/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_socketio import SocketIO, disconnect
-from flask_restful import Api
-from flask import request
-
-socketio = SocketIO(async_mode="gevent")
-
-api = Api(prefix="/api")
--- src/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_socketio import SocketIO, disconnect
-from flask_restful import Api
-from flask import request
-
-socketio = SocketIO(async_mode="gevent")
-
-api = Api(prefix="/api")
-
-__all__ = ['socketio', 'api', 'request', 'disconnect']
